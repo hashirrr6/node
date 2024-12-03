c8bd9a57a7b490bc6aac9056f9d2ac2ef81f173c
hashirrr6 <hashi3904@gmail.com>
2024-12-03 11:31:39 +0530

    s

--- works/works.js
+++ works/works.js
@@ -1,8 +1,21 @@
+const http=require("http")
 const fs=require("fs")
-
-fs.writeFile("home.html","hello",(error)=>{
-    if (error){
-        console.log("unable to write");
-        
-    }
+const url=require("url")
+// const { error } = require("console")
+http.createServer((req,res)=>{
+    const parsed=url.parse(req.url)
+    fs.readFile("./pages"+parsed.pathname,(error,data)=>{
+        if (error){
+            res.writeHead(404,{"Content-Type":"text/html"});
+            res.end("Error page not found")            
+        }
+        else{
+            res.writeHead(200,{"Content-Type":"text/html"});
+            res.write(data);
+            res.end();
+        }
+    })
+}).listen(3000,()=>{
+    console.log("Server Created");
+    
 })
